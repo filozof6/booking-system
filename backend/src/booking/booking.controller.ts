7346a8973543151b26d982ad48d015842a8bc3f3
“Roman <rzfilozof@gmail.com>
2025-07-14 15:22:27 -0500

    added zod validation to our booking data

--- backend/src/booking/booking.controller.ts
+++ backend/src/booking/booking.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { Booking } from './booking.entity';
+import { ZodValidationPipe } from './zod-validation-pipe';
+import { createBookingSchema } from '../utils/zod-booking-schema';
 
 @Controller('bookings')
 export class BookingController {
   constructor(private readonly bookingService: BookingService) {}
 
   @Post()
+  @UsePipes(new ZodValidationPipe(createBookingSchema))
   create(@Body() body: Partial<Booking>) {
     return this.bookingService.create(body);
   }
---
+++ backend/src/utils/zod-booking-schema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const createBookingSchema = z
+  .object({
+    fullName: z.string(),
+    phoneNumber: z.string(),
+    dateFrom: z.string(), // z.iso.datetime
+    dateTo: z.string(),
+    guests: z.number(),
+    bedrooms: z.number(),
+    bathrooms: z.number(),
+  })
+  .required();
+
+//export type CreateBookingDto = z.infer<typeof createBookingSchema>;
+// asdfsadf
